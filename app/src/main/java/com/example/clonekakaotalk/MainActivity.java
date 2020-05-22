3cdedb727b7ca8ca363c72e9b9b4ef10d9afe2ce
flowertaekk-dev <flowertaekk.dev@gmail.com>
2020-05-22 22:37:04 +0900

    first commit

---
+++ app/src/main/java/com/example/clonekakaotalk/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.clonekakaotalk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
