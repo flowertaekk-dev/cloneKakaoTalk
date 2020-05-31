7b4d4cf0e7b1b02e7f428739dcba670b3468fa91
flowertaekk-dev <flowertaekk.dev@gmail.com>
2020-05-31 16:24:13 +0900

    implements GO BACK functionality on ProfileDetailActivity

--- app/src/main/java/com/example/clonekakaotalk/ProfileDetailActivity.java
+++ app/src/main/java/com/example/clonekakaotalk/ProfileDetailActivity.java
@@ -2,6 +2,8 @@ package com.example.clonekakaotalk;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,19 +13,24 @@ import com.example.clonekakaotalk.utils.footer.FragmentFriendsList;
 
 public class ProfileDetailActivity extends AppCompatActivity {
 
+    private ImageButton _goBackButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_detail);
 
         Intent intent = getIntent();
-        initProfileDataFromIntent(intent);
+        _initProfileDataFromIntent(intent);
+
+        // init
+        _initGoBackButton();
     }
 
     /**
      * initialize profile data with parcel.
      */
-    private void initProfileDataFromIntent(Intent intent) {
+    private void _initProfileDataFromIntent(Intent intent) {
         if (intent != null) {
             Bundle bundle = intent.getExtras();
             Profile profile = bundle.getParcelable(FragmentFriendsList.PROFILE_FROM_FRIENDS_LIST_ACTIVITY);
@@ -33,5 +40,21 @@ public class ProfileDetailActivity extends AppCompatActivity {
                 profileNickNameView.setText(profile.getNickname());
             }
         }
+    }
+
+    // ---------------------------------------------------------------------------------------------
+    // OnClickedHandler
+
+    /**
+     * Go back to previous screen.
+     */
+    private void _initGoBackButton() {
+        _goBackButton = findViewById(R.id.profile_detail_return_button);
+        _goBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
